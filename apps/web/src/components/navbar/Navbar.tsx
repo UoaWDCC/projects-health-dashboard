f31bbfadce452f41c703a5139659b5c1977f8632
Andy <andyhuang649@gmail.com>
2026-04-25 23:25:31 +1200

    refactor: move logo to public folder and use svg

--- apps/web/src/components/navbar/Navbar.tsx
+++ apps/web/src/components/navbar/Navbar.tsx
@@ -4,7 +4,6 @@ import { Role } from '@repo/db'
 import { ProfileDropdown } from './ProfileDropdown'
 import Image from 'next/image'
 import Link from 'next/link'
-import logo from './logo.png'
 import { Courier_Prime, Figtree } from 'next/font/google'
 
 const courierPrime = Courier_Prime({ subsets: ['latin'], weight: '700' })
@@ -34,7 +33,7 @@ export async function Navbar() {
     <header className="sticky top-0 flex items-center justify-between px-6 xl:px-12 h-16 bg-white/65 border-b border-gray-200">
       <div className="flex items-center gap-4 xl:gap-6">
         <Link href="/">
-          <Image src={logo} alt="WDCC Logo" />
+          <Image src="/logo.svg" alt="WDCC Logo" width={80} height={40} />
         </Link>
         <span
           className={`text-[16px] xl:text-[24px] text-wdcc-blue leading-none translate-y-[3px] ${courierPrime.className}`}
