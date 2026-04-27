197422b2b01a2e52e8457860c62385c1f0db65c8
xtncz <ascendus.dev@gmail.com>
2026-04-27 21:47:15 +1200

    style(header): add animations to webster

--- apps/web/src/components/ui/header.tsx
+++ apps/web/src/components/ui/header.tsx
@@ -64,7 +64,7 @@ export const Header: React.FC<HeaderProps> = ({
             alt="WDCC Webster mascot"
             width={320}
             height={320}
-            className="object-contain"
+            className="object-contain hover:scale-110 transition-all duration-500 ease-in-out hover:drop-shadow-2xl hover:-rotate-12"
           />
         </div>
       </div>
