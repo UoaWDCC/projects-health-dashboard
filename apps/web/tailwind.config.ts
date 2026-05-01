630adb03fba14401ddc3ceff3fbe3bc5257d1e8a
Ben Kee <ascendus.dev@gmail.com>
2026-05-01 15:03:07 +1200

    fix(tailwind): correct wrong colour hex codes

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -14,22 +14,22 @@ const config: Config = {
       colors: {
         wdcc: {
           // Light colours
-          purple: '#E9CFCC',
+          purple: '#E9CFFC',
           peach: '#FDE6CF',
           mint: '#D4F7ED',
-
+      
           // Dark colours
           blue: {
             light: '#CFE0FD',
             DEFAULT: '#077CF1',
           },
           orange: '#FFB05F',
-          kelvin: '#E333A3', // pink (zesty kelvin)
+          kelvin: '#E333A3', // pink
           grey: {
             light: '#9A9EB8',
             DEFAULT: '#5A5E7A',
           },
-          oshan: '#1F2031', // black (...)
+          oshan: '#1F2031', // black
         },
       },
     },
