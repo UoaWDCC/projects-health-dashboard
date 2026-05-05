76ffe3a4c789a3598d96851cce14f709bc40df90
Arnav Bhatiani <arnavbhatiani1904@gmail.com>
2026-05-05 15:35:45 +1200

    fix(migration): wrap grants in conditional block to check for service_role existence

--- packages/db/prisma/migrations/20260505024557_grant_service_role_public_schema/migration.sql
+++ packages/db/prisma/migrations/20260505024557_grant_service_role_public_schema/migration.sql
@@ -1,2 +1,8 @@
-GRANT USAGE ON SCHEMA public TO service_role;
-GRANT ALL ON ALL TABLES IN SCHEMA public TO service_role;
+DO $$
+BEGIN
+  IF EXISTS (SELECT FROM pg_roles WHERE rolname = 'service_role') THEN
+    GRANT USAGE ON SCHEMA public TO service_role;
+    GRANT ALL ON ALL TABLES IN SCHEMA public TO service_role;
+  END IF;
+END
+$$;
