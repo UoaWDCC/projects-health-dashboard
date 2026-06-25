4241217eba22ff53975cb661d4262d63f0ae5a9f
DhruvE1008 <deri361@aucklanduni.ac.nz>
2026-06-25 22:02:06 +1200

    feat(web): implemented mobile view of leaderboard page

---
+++ apps/web/src/components/ui/DesktopLeaderboardSections.tsx
@@ -0,0 +1,53 @@
+import LeaderboardRow from '@/components/ui/LeaderboardRow'
+import { WeeklyLeaderboard } from '@/lib/project/leaderboard'
+import ClientSuspense from '../utils/ClientSuspense'
+
+interface DesktopLeaderboardSectionsProps {
+  data: WeeklyLeaderboard | null
+  loading: boolean
+}
+
+export default function DesktopLeaderboardSections({
+  data,
+  loading,
+}: DesktopLeaderboardSectionsProps) {
+  return (
+    <div className="flex justify-between px-5 sm:px-10 lg:px-20 gap-[21px] my-20 w-full">
+      <section className="flex flex-col gap-3 w-[415px]">
+        <h2 className="flex items-center gap-3 text-lg font-semibold">
+          <span className="w-[4.9px] h-5 rounded-full bg-wdcc-kelvin" />
+          Lines of Code Changed
+        </h2>
+        <ClientSuspense loading={loading} fallback={<p>Loading...</p>}>
+          {data?.linesOfCode.map(({ entry, theme }) => (
+            <LeaderboardRow key={entry.projectId} entry={entry} theme={theme} />
+          ))}
+        </ClientSuspense>
+      </section>
+
+      <section className="flex flex-col gap-3 w-[415px]">
+        <h2 className="flex items-center gap-3 text-lg font-semibold">
+          <span className="w-[4.9px] h-5 rounded-full bg-wdcc-blue" />
+          Commits Made
+        </h2>
+        <ClientSuspense loading={loading} fallback={<p>Loading...</p>}>
+          {data?.commits.map(({ entry, theme }) => (
+            <LeaderboardRow key={entry.projectId} entry={entry} theme={theme} />
+          ))}
+        </ClientSuspense>
+      </section>
+
+      <section className="flex flex-col gap-3 w-[415px]">
+        <h2 className="flex items-center gap-3 text-lg font-semibold">
+          <span className="w-[4.9px] h-5 rounded-full bg-wdcc-amber" />
+          Pull Requests Merged
+        </h2>
+        <ClientSuspense loading={loading} fallback={<p>Loading...</p>}>
+          {data?.merges.map(({ entry, theme }) => (
+            <LeaderboardRow key={entry.projectId} entry={entry} theme={theme} />
+          ))}
+        </ClientSuspense>
+      </section>
+    </div>
+  )
+}
