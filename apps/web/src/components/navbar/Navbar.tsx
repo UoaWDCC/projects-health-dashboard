c82ad9a08e0982ad7663d088303f01b5770cff50
Devin Shen <158394105+BakedAlaskaTM@users.noreply.github.com>
2026-07-30 21:13:11 +1200

    feat: created mobile navbar and header (#235)

--- apps/web/src/components/navbar/Navbar.tsx
+++ apps/web/src/components/navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import { getUserRoles } from '@/lib/auth'
 import { Role } from '@repo/db'
 import { ProfileDropdown } from './ProfileDropdown'
 import { NavbarBrand } from './NavbarBrand'
+import { NavbarMobile } from './NavbarMobile'
 import Link from 'next/link'
 
 export async function Navbar() {
@@ -26,50 +27,55 @@ export async function Navbar() {
     : null
 
   return (
-    <header className="z-50 sticky top-0 flex items-center justify-between px-5 sm:px-10 lg:px-20 h-16 bg-white/65 border-b border-gray-200 backdrop-blur-sm">
-      <NavbarBrand />
-      <div className="flex items-center gap-4 xl:gap-10 text-[16px]">
-        <div className="hidden lg:flex gap-4 xl:gap-8 font-figtree ">
-          <Link href="/" className="group flex items-center">
-            <span className="inline-block -translate-x-0.5 transition-transform duration-200 ease-out group-hover:translate-x-1">
-              {'('}
-            </span>
-            <span className="px-1">Projects</span>
-            <span className="inline-block translate-x-0.5 transition-transform duration-200 ease-out group-hover:-translate-x-1">
-              {')'}
-            </span>
-          </Link>
-          <Link href="/leaderboard" className="group flex items-center">
-            <span className="inline-block -translate-x-0.5 transition-transform duration-200 ease-out group-hover:translate-x-1">
-              {'('}
-            </span>
-            <span className="px-1">Leaderboard</span>
-            <span className="inline-block translate-x-0.5 transition-transform duration-200 ease-out group-hover:-translate-x-1">
-              {')'}
-            </span>
-          </Link>
-        </div>
+    <>
+      <header className="z-50 sticky top-0 flex items-center justify-between px-5 sm:px-10 lg:px-20 h-16 bg-white/65 border-b border-gray-200 backdrop-blur-sm">
+        <NavbarBrand />
+        <div className="flex items-center gap-4 xl:gap-10 text-[16px]">
+          <div className="hidden lg:flex gap-4 xl:gap-8 font-figtree ">
+            <Link href="/" className="group flex items-center">
+              <span className="inline-block -translate-x-0.5 transition-transform duration-200 ease-out group-hover:translate-x-1">
+                {'('}
+              </span>
+              <span className="px-1">Projects</span>
+              <span className="inline-block translate-x-0.5 transition-transform duration-200 ease-out group-hover:-translate-x-1">
+                {')'}
+              </span>
+            </Link>
+            <Link href="/leaderboard" className="group flex items-center">
+              <span className="inline-block -translate-x-0.5 transition-transform duration-200 ease-out group-hover:translate-x-1">
+                {'('}
+              </span>
+              <span className="px-1">Leaderboard</span>
+              <span className="inline-block translate-x-0.5 transition-transform duration-200 ease-out group-hover:-translate-x-1">
+                {')'}
+              </span>
+            </Link>
+          </div>
 
-        <div className="flex items-center gap-2 xl:gap-4 font-arial text-[16px]">
-          {(isExec || isAdmin) && (
-            <Link
-              href="/exec-dashboard"
-              className="hidden lg:block rounded-full bg-wdcc-oshan text-white hover:bg-gray-300 px-4 py-1.5"
-            >
-              Exec dashboard
-            </Link>
-          )}
-          {isAdmin && (
-            <Link
-              href="/admin-dashboard"
-              className="hidden lg:block rounded-full bg-wdcc-oshan text-white hover:bg-gray-300 px-4 py-1.5"
-            >
-              Admin dashboard
-            </Link>
-          )}
-          <ProfileDropdown user={profileUser} />
+          <div className="flex items-center gap-2 xl:gap-4 font-arial text-[16px]">
+            {(isExec || isAdmin) && (
+              <Link
+                href="/exec-dashboard"
+                className="hidden lg:block rounded-full bg-wdcc-oshan text-white hover:bg-gray-300 px-4 py-1.5"
+              >
+                Exec dashboard
+              </Link>
+            )}
+            {isAdmin && (
+              <Link
+                href="/admin-dashboard"
+                className="hidden lg:block rounded-full bg-wdcc-oshan text-white hover:bg-gray-300 px-4 py-1.5"
+              >
+                Admin dashboard
+              </Link>
+            )}
+            <ProfileDropdown user={profileUser} />
+          </div>
         </div>
+      </header>
+      <div className="lg:hidden">
+        <NavbarMobile />
       </div>
-    </header>
+    </>
   )
 }
