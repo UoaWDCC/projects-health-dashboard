60b72adb9efdf5a880e9e039838c1e5cb68de090
NoobCrewDelux <j0sephw1532@gmail.com>
2026-08-04 17:10:09 +1200

    fix: padding bugs

--- apps/web/src/components/ui/LiveCommitFeed.tsx
+++ apps/web/src/components/ui/LiveCommitFeed.tsx
@@ -8,7 +8,7 @@ export default function LiveCommitFeed() {
 
   return (
     <div className="flex rounded-3xl border-2 border-wdcc-grey/10 lg:border lg:border-white/50 w-full mx-auto flex-col bg-[#FAFBFC] lg:bg-white/70">
-      <div className="flex flex-row items-center gap-2 lg:gap-5 pl-2 pt-5 lg:pb-3">
+      <div className="flex flex-row items-center gap-2 lg:gap-5 pl-5 lg:pl-8 py-3 lg:py-4 ">
         <svg
           width="12"
           height="12"
@@ -19,7 +19,7 @@ export default function LiveCommitFeed() {
         >
           <rect width="12" height="12" rx="5.90908" fill="#16A34A" />
         </svg>
-        <p className="font-mono font-light tracking-widest lg:tracking-normal lg:font-medium text-sm lg:text-lg text-wdcc-grey">
+        <p className="font-mono font-light tracking-widest lg:tracking-normal lg:font-medium text-sm lg:text-xl text-wdcc-grey">
           LIVE COMMITS
         </p>
       </div>
